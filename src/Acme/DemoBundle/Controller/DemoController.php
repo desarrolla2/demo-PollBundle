121926405f0802db1c12d61d7e10b528a6653815
Daniel González <daniel.gonzalez@freelancemadrid.es>
2012-03-30 09:22:05 +0200

    refs, initial import

---
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DemoController extends Controller
+{
+    /**
+     * @Route("/", name="_demo")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+
+}
